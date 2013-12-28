f11bcbe25b00929497b72a629d6d3018b09f613f
Grzegorz Zajaczkowski <gzajaczkowski@gmail.com>
2013-12-28 02:15:21 +0100

    Dodanie gem spork

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
+
+
 describe "Static pages" do
+  let(:tit) {"Ruby on Rails Tutorial Sample App |"}
 
   describe "Home page" do
 
@@ -11,7 +14,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Home")
     end
   end
 
@@ -24,7 +27,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{tit}  Help")
     end
   end
 
@@ -37,7 +40,20 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{tit}  About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{tit}  Contact")
     end
   end
 end
